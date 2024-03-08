cad130950ee86cbb7125bb13a00359daea941ca4
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-08 15:54:29 +0000

    Disable spotlight hotkeys

--- nix-darwin/bootstrap.nix
+++ nix-darwin/bootstrap.nix
@@ -1,17 +1,13 @@
-{ lib, ... }:
-let
-  hotkeys = [
+{ lib, ... }: {
+  enable = true;
+  text = let
+    hotkeys = [
       64
       65
     ];
-    disableHotKeyCommands = map (key:
-      ''
-        /usr/libexec/PlistBuddy $HOME/Library/Preferences/com.apple.symbolichotkeys.plist -c 'Add AppleSymbolicHotKeys:${toString key}:enabled bool false
-      '') hotkeys;
-in {
-  enable = true;
-  text = ''
-    echo >&2 "Disabling hotkeys..."
-    ${lib.concatStringsSep "\n" disableHotKeyCommands}
-  '';
+    disableHotKeyCommands = map (key: "/usr/libexec/PlistBuddy $HOME/Library/Preferences/com.apple.symbolichotkeys.plist -c 'Add AppleSymbolicHotKeys:${toString key}:enabled bool false") hotkeys;
+    in ''
+      echo "Disabling hotkeys..."
+      ${lib.concatStringsSep "\n" disableHotKeyCommands}
+    '';
 }
