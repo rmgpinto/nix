f1ee6c4e824787b1044a5180491912bf52bed2b4
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-08 21:59:26 +0000

    Change activation scripts

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -13,7 +13,7 @@
       64
       65
     ];
-    disableHotKeyCommands = map (key: "/usr/libexec/PlistBuddy $HOME/Library/Preferences/com.apple.symbolichotkeys.plist -c 'Add AppleSymbolicHotKeys:${toString key}:enabled bool false") hotkeys;
+    disableHotKeyCommands = map (key: "/usr/libexec/PlistBuddy ${homeDirectory}/Library/Preferences/com.apple.symbolichotkeys.plist -c 'Add AppleSymbolicHotKeys:${toString key}:enabled bool false") hotkeys;
     in ''
       echo "Disabling hotkeys..."
       ${lib.concatStringsSep "\n" disableHotKeyCommands}
