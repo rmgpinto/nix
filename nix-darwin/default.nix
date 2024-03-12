67e0a929fff67e15959550aee8412af90b335031
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-12 14:43:34 +0000

    Change dock

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -80,7 +80,7 @@
       persistent-apps = [
         "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
         "/System/Applications/Mail.app"
-        # "${homeDirectory}/Applications/Home\ Manager\ Apps/Notion.app"
+        "/Applications/Home\ Manager\ Apps/Notion.app"
         "${pkgs.vscode}/Applications/Visual\ Studio\ Code.app"
         "${pkgs.alacritty}/Applications/Alacritty.app"
         "${pkgs.slack}/Applications/Slack.app"
