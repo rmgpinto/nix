7f1c381ce10bf544f428408ac8f38978b0449ea6
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-15 08:13:12 +0000

    Change vscode

--- home-manager/vscode_macos.nix
+++ home-manager/vscode_macos.nix
@@ -1,12 +1,10 @@
 { pkgs, os }: {
   enable = true;
   enableExtensionUpdateCheck = true;
+  mutableExtensionsDir = true;
   extensions = (with pkgs.vscode-extensions; [
     hashicorp.terraform
     b4dm4n.vscode-nixpkgs-fmt
   ]);
-  globalSnippets = { };
-  keybindings = [ ];
-  mutableExtensionsDir = false;
   userSettings = import ./vscode_settings_${os}.nix;
 }
