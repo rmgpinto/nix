44b8b489fd772a03069cd131fb62509e0916f8cc
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-18 16:16:25 +0000

    Change pam sudo

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -199,7 +199,6 @@
     ];
     etc."pam.d/sudo_local".text = ''
       auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
-      auth       sufficient     pam_tid.so
     '';
   };
 
