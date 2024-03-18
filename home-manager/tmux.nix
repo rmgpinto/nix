1a94f852801820c3a67344a3d9da59ecbeeadb97
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-18 13:56:52 +0000

    Change tmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -3,4 +3,16 @@
   baseIndex = 1;
   clock24 = true;
   mouse = true;
+  plugins = with pkgs; [
+    {
+      plugin = tmuxPlugins.catppuccin;
+      extraConfig = '' 
+        set -g @catppuccin_flavour 'mocha'
+        set -g @catppuccin_window_tabs_enabled on
+        set -g @catppuccin_date_time "%H:%M%S"
+      '';
+    }
+  ];
+  extraConfig = ''
+  '';
 }
