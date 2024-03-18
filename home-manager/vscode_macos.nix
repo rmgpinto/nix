3075393cb2c3a377d5199d4ce1bf7f539ae43f1b
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-18 08:20:20 +0000

    vscode color

--- home-manager/vscode_macos.nix
+++ home-manager/vscode_macos.nix
@@ -10,6 +10,18 @@
     #   sha256 = "";
     # nix will complain about the sha256 being empty, but it will be available in the output.
     (pkgs.vscode-utils.extensionFromVscodeMarketplace {
+      name = "catppuccin-vsc";
+      publisher = "catppuccin";
+      version = "3.13.0";
+      sha256 = "sha256-z6sQhC0V6j2ws9AyQE6eaTehj+2PpDjDOplB99aTPY8=";
+    })
+    (pkgs.vscode-utils.extensionFromVscodeMarketplace {
+      name = "catppuccin-vsc-icons";
+      publisher = "catppuccin";
+      version = "1.10.0";
+      sha256 = "sha256-6klrnMHAIr+loz7jf7l5EZPLBhgkJODFHL9fzl1MqFI=";
+    })
+    (pkgs.vscode-utils.extensionFromVscodeMarketplace {
       name = "terraform";
       publisher = "hashicorp";
       version = "2.30.2024022914";
