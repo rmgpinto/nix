5d5d1b44b727fcf05134f5e47c8b992e4d928178
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-21 15:34:15 +0000

    Change direnv

--- home-manager/env.nix
+++ home-manager/env.nix
@@ -1,3 +1,7 @@
 { username }: {
   PATH = "/etc/profiles/per-user/${username}/bin:$PATH";
+  # Disable direnv warnings
+  DIRENV_WARN_TIMEOUT = 0;
+  # Disable direnv log
+  DIRENV_LOG_FORMAT = "";
 }
