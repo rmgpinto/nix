e55700b658c5125fc955dfc0fbeb1c483b286bbd
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-24 22:41:15 +0000

    Change terraform dev shell

--- dev-shells/terraform/flake.nix
+++ dev-shells/terraform/flake.nix
@@ -4,19 +4,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgs-terraform }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        terraform = nixpkgs-terraform.packages.${system}."1.6.4";
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            terraform
+          shellHook = ''
+            export TENV_ROOT=${pkgs.tenv}/bin/versions
+            sudo mkdir -p ''${TENV_ROOT}/Terraform
+            USER=$(whoami)
+            sudo chown -R ''${USER}:staff ''${TENV_ROOT}
+          '';
+          buildInputs = with pkgs; [
+            tenv
           ];
         };
       });
