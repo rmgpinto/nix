3a244717773e794086a02b637e30443e90fe3d9a
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-26 17:28:19 +0000

    Change tmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -6,10 +6,11 @@
   clock24 = true;
   mouse = true;
   keyMode = "vi";
+  aggressiveResize = true;
   plugins = with pkgs; [
     {
       plugin = tmuxPlugins.catppuccin;
-      extraConfig = '' 
+      extraConfig = ''
         set -g @catppuccin_flavour 'mocha'
         set -g @catppuccin_window_tabs_enabled on
         set -g @catppuccin_date_time "%H:%M:%S"
@@ -17,6 +18,7 @@
     }
   ];
   extraConfig = ''
+    set -g renumber-windows on
     set -g status-position 'top'
   '';
 }
