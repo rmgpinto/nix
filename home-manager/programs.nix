6bc38b918d2830cd8d4015dc48266bc419747ed4
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-27 12:36:30 +0000

    Add lazygit

--- home-manager/programs.nix
+++ home-manager/programs.nix
@@ -40,6 +40,23 @@
 
   git = import ./git.nix { inherit pkgs git; };
 
+  lazygit = {
+    enable = true;
+    settings = {
+      theme = {
+        activeBorderColor = [ "#89b4fa" "bold" ];
+        inactiveBorderColor = [ "#a6adc8" ];
+        optionsTextColor = [ "#89b4fa" ];
+        selectedLineBgColor = [ "#313244" ];
+        cherryPickedCommitBgColor = [ "#45475a" ];
+        cherryPickedCommitFgColor = [ "#89b4fa" ];
+        unstagedChangesColor = [ "#f38ba8" ];
+        defaultFgColor = [ "#cdd6f4" ];
+        searchingActiveBorderColor = [ "#f9e2af" ];
+      };
+    };
+  };
+
   gpg = {
     enable = true;
   };
