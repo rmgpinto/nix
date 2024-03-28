663880e38651921ba2bc8f481f5ab79cb98f8161
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-28 08:56:54 +0000

    Add rails dev shell

---
+++ dev-shells/rails/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "rails devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ruby_3_2
+            rubyPackages_3_2.rails
+          ];
+        };
+      });
+}
