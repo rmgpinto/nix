d92a5cc4e456cf7b67bc3d9dccc36fe406f296dc
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-03-28 16:45:27 +0000

    Fix rails dev shell

--- dev-shells/rails/flake.nix
+++ dev-shells/rails/flake.nix
@@ -10,15 +10,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        gemHome = "$HOME/.gem/ruby/${builtins.baseNameOf pkgs.ruby}";
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            libyaml
             ruby
+            libyaml
             rubyPackages.rails
           ];
+          # rails complains about error_highlight version
+          shellHook = ''
+            gem install error_highlight -v 0.6.0
+          '';
         };
       }
     );
